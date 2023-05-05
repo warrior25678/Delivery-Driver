5ea6e4790853fd30aef9a9547cf173d74fc3c8b6
Matthew Lonar <matthewlonar@gmail.com>
2023-05-05 16:42:56 -0300

    Updated script to modify car color when they have a package and go back to normal when they don't.

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32(1,1,1,1);
+    [SerializeField] Color32 noPackageColor = new Color32(1,1,1,1);
     bool hasPackage = false;
+
+    SpriteRenderer spriteRenderer;
+
+    private void Start() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();    
+    }
     void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("Damn it!");
@@ -16,12 +25,14 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package picked up");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject);
         } 
 
         else if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package Delivered");
+            spriteRenderer.color = noPackageColor;
             hasPackage = false;
         }
     }
