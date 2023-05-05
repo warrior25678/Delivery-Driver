362deaaab735d00d06123f202b9ba466b0af93b4
Matthew Lonar <matthewlonar@gmail.com>
2023-05-05 16:14:26 -0300

    Added basic package pick up and delivery logic.

---
+++ Assets/Scripts/Delivery.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    bool hasPackage = false;
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        Debug.Log("Damn it!");
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.tag == "Package")
+        {
+            Debug.Log("Package picked up");
+            hasPackage = true;
+        } 
+
+        else if (other.tag == "Customer" && hasPackage)
+        {
+            Debug.Log("Package Delivered");
+            hasPackage = false;
+        }
+    }
+}
